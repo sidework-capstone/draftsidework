af3facad8df7815faeb2e32e79127c77ab9f5600
Mary Kathryn Syms <syms@ladytacos.com>
2018-01-29 14:42:31 -0600

    changed package name

--- src/main/java/com/codeup/sidework/HelloWorldController.java
+++ src/main/java/com/codeup/sidework/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.codeup.sequoiaspringbootblog;
+package com.codeup.sidework;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/codeup/sidework/WebApplication.java
+++ src/main/java/com/codeup/sidework/WebApplication.java
@@ -1,4 +1,4 @@
-package com.codeup.sequoiaspringbootblog;
+package com.codeup.sidework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
