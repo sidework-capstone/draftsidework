cd546d1c9b9995044cae964a759324c023517d59
MicCabeza22 <cabeza.michael@yahoo.com>
2018-01-30 16:25:51 -0600

    Committing changes...

---
+++ src/main/java/com/codeup/sidework/SecurityConfiguration.java
@@ -0,0 +1,8 @@
+package com.codeup.sidework;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+}
