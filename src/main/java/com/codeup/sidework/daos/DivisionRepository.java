08743e91bb3238e8c8914f22d282c2b0406e2602
Mary Kathryn Syms <syms@ladytacos.com>
2018-02-06 09:44:29 -0600

    removed files: Seeder, Division, Department

--- src/main/java/com/codeup/sidework/daos/DivisionRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.codeup.sidework.daos;
-//
-//import com.codeup.sidework.models.Division;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface DivisionRepository extends CrudRepository<Division, Long> {
-//        Division findByDivisionName(String divisionName);
-//    }
