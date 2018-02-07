7b6da4dec025e01b5502152607e7762cfd552fdc
MicCabeza22 <cabeza.michael@yahoo.com>
2018-02-07 09:58:11 -0600

    Attempting to save a new business and assing it to logged in user...

--- src/main/java/com/codeup/sidework/controllers/BusinessesController.java
+++ src/main/java/com/codeup/sidework/controllers/BusinessesController.java
@@ -1,41 +1,43 @@
 package com.codeup.sidework.controllers;
 
+import com.codeup.sidework.daos.BusinessesRepository;
 import com.codeup.sidework.daos.UserRepository;
 import com.codeup.sidework.models.Business;
-import com.codeup.sidework.daos.BusinessesRepository;
 import com.codeup.sidework.models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BusinessesController {
-    private UserRepository userRepository;
     private final BusinessesRepository businessesRepository;
+    private final UserRepository userRepository;
 
-    public BusinessesController(BusinessesRepository businessesRepository) {
+    public BusinessesController(BusinessesRepository businessesRepository, UserRepository userRepository) {
         this.businessesRepository = businessesRepository;
+        this.userRepository = userRepository;
     }
 
-    @GetMapping("/users/register-mgmt")
-    public String showManagementRegisterForm(Model model) {
+    @GetMapping("/businesses/create")
+    public String showCreateBusinessForm(Model model) {
         model.addAttribute("business", new Business());
 
-        return "users/register-mgmt";
+        return "businesses/create";
     }
 
     @PostMapping("/businesses/create")
     public String saveNewBusiness(@ModelAttribute Business business) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-        business.setUser(businessesRepository.findOne(user.getId()));
+        business.setUser(userRepository.findOne(user.getId()));
 
         businessesRepository.save(business);
-        return "redirect:/login";
+
+        return "redirect:/businesses/create";
     }
 
     @GetMapping("/users/workspace-mgmt")
@@ -43,8 +45,12 @@ public class BusinessesController {
         return "users/workspace-mgmt";
     }
 
-    @GetMapping("/users/profile-business")
-    public String viewBusinessProfile() {
-        return "users/profile-business";
+    @GetMapping("/businesses/profile/{id}")
+    public String viewBusinessProfile(@PathVariable long id, Model model) {
+        Business business = businessesRepository.findOne(id);
+
+        model.addAttribute("business", business);
+
+        return "businesses/profile";
     }
 }
