9408bc4f3e6ca1c5d17219d6314040d524a0eceb
Mary Kathryn Syms <syms@ladytacos.com>
2018-02-07 23:31:12 -0600

    fixed compiler error but now getting error of duplicate code

--- src/main/java/com/codeup/sidework/controllers/BusinessesController.java
+++ src/main/java/com/codeup/sidework/controllers/BusinessesController.java
@@ -5,8 +5,6 @@ import com.codeup.sidework.daos.UserRepository;
 import com.codeup.sidework.models.Business;
 import com.codeup.sidework.models.User;
 import com.codeup.sidework.services.BusinessesService;
-import com.codeup.sidework.services.ListingsService;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
