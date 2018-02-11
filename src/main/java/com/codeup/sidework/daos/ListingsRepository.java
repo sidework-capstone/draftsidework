f04d791ff07c2e3509c24ed288fe66c3e6fa4601
brandongossen <brandongossen@yahoo.com>
2018-02-11 08:01:25 -0600

    Search Bar - Listings
    -Connect the search bar method for businesses with the listings/index view.
    
    -Allow the user to search listings by listing title and description.

--- src/main/java/com/codeup/sidework/daos/ListingsRepository.java
+++ src/main/java/com/codeup/sidework/daos/ListingsRepository.java
@@ -2,10 +2,17 @@ package com.codeup.sidework.daos;
 
 import com.codeup.sidework.models.Business;
 import com.codeup.sidework.models.Listing;
+import com.codeup.sidework.models.Worker;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ListingsRepository extends CrudRepository<Listing, Long> {
     List<Listing> findAllByBusiness(Business business);
+
+    @Query(value = "SELECT * FROM listings l WHERE " + "LOWER(l.title) LIKE LOWER(CONCAT('%',:keyword, '%')) OR " + "LOWER(l.description) LIKE LOWER(CONCAT('%',:keyword, '%'))",
+            nativeQuery = true)
+    List<Listing> search(@Param("keyword") String keyword);
 }
